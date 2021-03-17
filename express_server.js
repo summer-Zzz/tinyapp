4c855b2f5fcc50ad49a8de0a2827c7f4f01cd50f
Hongjin Zhu <summerellaooo@gmail.com>
2021-03-17 22:07:08 +0000

    add login template

--- express_server.js
+++ express_server.js
@@ -40,9 +40,9 @@ const findUserByEmail = (email) => {
 }
 
 const authenticateUser = (email, password) => {
-  const foundUser = findUserByEmail(email);
-  if(foundUser && userFound.password === password){
-    return userFound;
+  const userId = findUserByEmail(email);
+  if(userId.password === password){
+    return true;
   } else {
     return false;
   }
@@ -59,7 +59,7 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = 
   { urls: urlDatabase,
-    user: req.cookies["user"]
+    user: req.cookies["user_id"]
   };
   res.render("urls_index", templateVars);
   // to loop through the database
@@ -71,21 +71,20 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newShortUrl}`);
 })
 
+  //delete the shortened url
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL
-  //delete the shortened url
   delete urlDatabase[shortURL];
   //redirect
   res.redirect('/urls');
 })
 
+  //create new url
 app.get("/urls/new", (req, res) => {
-  const templateVars = 
-  {
-    user: req.cookies["user"]
+  const templateVars = {
+    user: req.cookies["user_id"]
   };
   res.render("urls_new", templateVars);
-  //create new url
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -93,50 +92,63 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = 
   { shortURL: shortURL, 
     longURL: urlDatabase[shortURL],
-    user: req.cookies["user"] };
+    user: req.cookies["user_id"] };
   res.render("urls_show", templateVars);
 });
 
+  //redirect to the longURL page by using a shortURL
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL; 
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
-  //redirect to the longURL page by using a shortURL
 });
 
-// app.get("/urls/:shortURL", (req, res) => {
-//   const templateVars = 
-//   {shortURL: req.params.shortURL,
-//     longURL: req.query.longURL}
-//   res.render('urls_show', templateVars);
-// })
-
-
+ //update a url
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
-  // const longURL = urlDatabase[shortURL];
-  // updateURL(shortURL, longURL);
   urlDatabase[shortURL] = longURL;
   res.redirect('/urls');
   return;
-  //update a url
 })
 
-// app.post("/login", (req, res) => {
-//   res.cookie('username', req.body.username);
-//   res.redirect("/urls");
-// })
+app.get("/login", (req, res) => {
+  const templateVars = {
+    user: req.cookies["user_id"]
+  };
+  res.render("urls_login", templateVars);
+})
+
+//login
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const userFound = findUserByEmail(email);
+  if (email && password){
+    if (userFound) {
+      if (authenticateUser(email, password)) {
+        res.cookie('user_id', findUserByEmail(email));
+        res.redirect("/urls");
+      } else {
+        res.status(403).send('Wrong Password!');
+      }
+    } else {
+      res.status(403).send('User Not Found');
+    }
+  } else {
+    res.status(403).send('Put Something Here!');
+  }
+});
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('user');
+  res.clearCookie('user_id');
   res.redirect("/urls");
 })
 
 app.get("/register", (req, res) => {
   //render the regitser template
   const templateVars = {
-    user: req.cookies["user"]
+    user: req.cookies["user_id"]
   };
   res.render("urls_register", templateVars)
 })
@@ -144,19 +156,19 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   //the endpoint that handles the registration form data
   const newUserId = generateRandomString();
-  const newEmail = req.body.email;
-  const newPassword = req.body.password;
+  const newEmail = req.body.newemail;
+  const newPassword = req.body.newpassword;
   console.log(users)
   const userFound = findUserByEmail(newEmail);
   if (newEmail && newPassword) { //check if they put email and password
-    if (!userFound ) { // check if user exist
+    if (!userFound ) { // check if user exists
       users[newUserId] = 
       {
         id: newUserId,
         email: newEmail,
         password: newPassword
       }
-      res.cookie('user', users[newUserId]);
+      res.cookie('user_id', users[newUserId]);
       res.redirect('/urls');
     } else {
       res.status(400).send('The user already exists!');
